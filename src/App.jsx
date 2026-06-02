2289c7f728961d1a4afab7cfe13bbd2bee642c3e
NithissKumaar <nithisskumaar0920@gmail.com>
2026-06-02 15:39:40 +0530

    final updated for login and navbar implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Table from './components/table'
+import { BrowserRouter, Routes, Route, Link , Navigate } from 'react-router-dom';
+
 import './App.css'
-import Login from "./pages/login";
-import Dashboard from "./pages/dashboard";
-import Users from "./pages/user";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Navbar from './components/Navbar';
 
 function App() {
   
@@ -11,17 +12,12 @@ function App() {
   return (
     <>
     <BrowserRouter>
-      
-     
-
       <Routes>
-
         <Route path="/" element={<Login />} />
-
         <Route path="/dashboard" element={<Dashboard />} />
-
         <Route path="/users" element={<Users />} />
-
+        {/* Fallback redirect */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
       
