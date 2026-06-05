df0a3decd96f021867566f89b412f3b7ab859ed3
NithissKumaar <nithisskumaar0920@gmail.com>
2026-06-05 22:09:34 +0530

    Day-5: Final Route setup for Employe Menu

---
+++ src/constants/relationOptions.jsx
@@ -0,0 +1,6 @@
+export const relations = [
+  "Father",
+  "Mother",
+  "Brother",
+  "Sister",
+];
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import Users from "../pages/Users";
 import Profile from "../pages/Profile";
+import Employe from "../pages/Employe";
 
 function AppRoutes() {
   return (
@@ -18,6 +19,7 @@ function AppRoutes() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
         <Route path="/profile" element={<Profile />} />
+        <Route  path="/employe"  element={<Employe/>} />
       </Route>
 
       
