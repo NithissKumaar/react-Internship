89a360514841080b47fe08a5bf515130fabb6e9e
NithissKumaar <nithisskumaar0920@gmail.com>
2026-06-17 17:28:44 +0530

    Day-17 : Footer Style updated

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -1,9 +1,9 @@
 import { Copyright } from "lucide-react";
 function Footer() {
   return (
-    <footer className="w-full bg-white  border-slate-200 py-1">
+    <footer className="w-full bg-white py-1 border-t border-slate-300 shadow-sm">
 
-      <div className=" px-6 h-7 flex items-center justify-between  bg-slate-100">
+      <div className=" px-6 h-5 flex items-center justify-between  bg-white ">
         <div className="mt-auto flex items-center justify-center gap-2">
             <Copyright size={12}/>
         <p className="text-sm text-slate-500">
